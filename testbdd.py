1f51a2485c93feb127bc37c5426fb1649bbec1d3
craig eales <craig.eales@gmail.com>
2011-12-25 11:13:05 +0000

    Added (C) notices and GPL

--- testbdd.py
+++ testbdd.py
@@ -1,3 +1,20 @@
+# Copyright 2011 Craig Eales
+
+# This file is part of py-simple-bdd.
+
+# py-simple-bdd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# py-simple-bdd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with py-simple-bdd.  If not, see <http://www.gnu.org/licenses/>.
+
 import bdd
 import unittest
 
